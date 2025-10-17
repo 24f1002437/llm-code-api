e2f1543e755cf8d4d51015098eb61fad8618b339
Mehnaz Shaikh <24f1002437@ds.study.iitm.ac.in>
2025-10-17 05:50:20 +0530

    Refactor Flask app to improve structure and clarity

--- app.py
+++ app.py
@@ -101,4 +101,6 @@ def evaluate():
 # ----------------------------
 if __name__ == "__main__":
     os.makedirs("temp", exist_ok=True)
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=5000)
+
+
